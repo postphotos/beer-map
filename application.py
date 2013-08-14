ec6d864201b12a8125bc19137a3cbbfab2da42ce
Matthew Gerring <gerring.matthew@gmail.com>
2013-08-13 20:59:35 -0600

    links & map work

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, Response
 import sys, os
+import gspread
+import json
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -10,9 +12,30 @@ try:
 except:
 	pass
 
+gdrive_client = gspread.login(app.config['GDRIVE_USER'], app.config['GDRIVE_PASSWORD'])
+
+def _get_json(document_key):
+	document = gdrive_client.open_by_key(document_key)
+	# get_all_records takes the first row of the spreadsheet and uses it
+	# as keys for a dict, which can be turned into valid json"""
+	data = document.sheet1.get_all_records()
+	#'dumps' is short for 'dump string'
+	return json.dumps(data)
+
+def _render_map(document_key, meta = None):
+	# This function should be used by all of the functions preceded
+	# by the 'route' decorator. Its
+	map_json = _get_json(document_key)
+	return render_template('map.html', map_json = map_json, meta = meta)
+
+
 @app.route('/', methods=['GET'])
 def home():
 	return render_template('home.html')
+
+@app.route('/map-one/', methods=['GET'])
+def map_one():
+	return _render_map('0AgryR3_lJEaVdEJqQ0dKTEU0VDlCLXFwNWNjS1BDa1E')
 
 if __name__ == '__main__':
 	app.run()
--- local_settings.py
+++ local_settings.py
@@ -1,1 +1,4 @@
 DEBUG = True
+
+GDRIVE_USER = 'gerring.matthew@gmail.com'
+GDRIVE_PASSWORD = 'chmod777yh!'
